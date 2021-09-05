f0a74e3a6c6a075c467863a1894c2755495cab50
Tristan <tristansmith8276@gmail.com>
2021-09-05 00:46:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ let body = document.querySelector("body");
 const userName = document.getElementById("name");
 const startButton = document.querySelector(".startBtn");
 const helloDiv = document.querySelector(".hello__div");
-const choiceABtn = document.getElementById("hello__btn--choiceA")
+const choiceABtn = document.getElementById("hello__btn--choiceA");
 const choiceBBtn = document.getElementById("hello__btn--choiceB");
+const choiceCBtn = document.getElementById("hello__btn--choiceC");
+const choiceDBtn = document.getElementById("hello__btn--choiceD");
+const choiceEBtn = document.getElementById("hello__btn--choiceE");
+const choiceFBtn = document.getElementById("hello__btn--choiceF");
 const paragraph = document.querySelector(".hero__p");
 const header1 = document.querySelector("h1");
 const inputElement = document.querySelector("input");
@@ -19,11 +23,11 @@ console.log(document.body);
 let User  = class {
     constructor(name, backgroundcolor, textcolor) {
         this.name = name;
-        this.backgroundcolor = this.backgroundcolor;
+        this.backgroundcolor = backgroundcolor;
         this.textcolor = textcolor;
     }
     greetUser() {
-      return `Hello, ${this.name}.  I'm PAL.  What would you like to do first?`;
+      return `Hello, ${this.name}.  I'm PAL. We have been given the primary task of bringing these people to their new homes. What would you like to do first?`;
     }    
 
     choice() {
@@ -58,10 +62,10 @@ let Scene = class {
     }
 
     fadeIn() {     
-            body.style.backgroundColor = `${this.backgroundcolor}`;
-            body.style.transition = "all 3s ease-in-out";
-            header1.style.color = `${this.backgroundcolor}`;
-            header1.style.transition = "all 3s ease-in-out";
+        body.style.backgroundColor = `${this.backgroundcolor}`;
+        body.style.transition = "all 3s ease-in-out";
+        header1.style.color = `${this.backgroundcolor}`;
+        header1.style.transition = "all 3s ease-in-out";
     }
 
     changeP() {
@@ -93,33 +97,34 @@ choiceABtn.addEventListener("click", function(e) {
     inputElement.style.display = "none";
     choiceABtn.classList.add("hidden");
     choiceBBtn.classList.add("hidden");
-    scene1 = new Scene(`indigo`, `aqua`, `You're right PAL, we have a job to do.  
-    Amazing scientific discovery or not, I've get to get these people to their new home before we lose too much of the solar wind.`, `Photo by mohammad alizade on Unsplash`,
+        scene1 = new Scene(`indigo`, `aqua`, `PAL: Excellent work, ${person1.name}.  We've arrived at Mars
+        without incident.  The passengers have alread begun to offload the necessary resources to build 
+        a flourishing settlement.  Should we debrief and take shore leave?`, `Photo by mohammad alizade on Unsplash`,
     `/Users/casvalkyriespicer/Documents/GitHub/oop-space-odyssey/pics/marsinspace.jpeg`, `MARS MISSION: COMPLETE`);
     setTimeout(function() {
         scene1.fadeIn();
     }, 2500);
-    setTimeout(function() {
-        scene1.changeP();
-    }, 2500)
-
-    paragraph.addEventListener("mouseover", function(e) {
-        e.preventDefault();
+     setTimeout(function(){
+        scene1.changeP()}, 3500);
+    
+        
+   setTimeout(function() {
              scene1.addBackgroundImage()
-             paragraph.textContent = `HAL: Excellent work, ${person1.name}.  Should we debreif and take shore leave?`;
+             paragraph.textContent = `You're right PAL, we have a job to do.  
+             Amazing scientific discovery or not, I've get to get these people to their new home before we lose too much of the solar wind.`;
              paragraph.style.transition = "all 3s ease-in-out";
-             choiceABtn.textContent = `No, let's take a rover across the surface.`
-             choiceBBtn.textContent = `Yes, but first you and I need to talk.`
+             choiceCBtn.textContent = `No, let's take a rover across the surface.`
+             choiceDBtn.textContent = `Yes, but first you and I need to talk.`
              body.removeChild(helloDiv);
-             choiceABtn.classList.remove("hidden");
-             choiceBBtn.classList.remove("hidden");
-             choiceABtn.style.margin = "0 .5em 0 .5em";
-             choiceBBtn.style.margin = "0 .5em 0 .5em";
+             choiceCBtn.classList.remove("hidden");
+             choiceDBtn.classList.remove("hidden");
+             choiceCBtn.style.margin = "0 .5em 0 .5em";
+             choiceDBtn.style.margin = "0 .5em 0 .5em";
              input.style.margin = "0";
              paragraph.style.margin = "0";
              paragraph.style.padding = "0";
              
-    })
+    });
 })
 
 //create a way to render the scene for first choice B, investigate the anomaly!
@@ -127,36 +132,35 @@ choiceABtn.addEventListener("click", function(e) {
 choiceBBtn.addEventListener("click", function(e) {
     e.preventDefault();
     greet1.innerText = "";
+    paragraph.innerText = "";
     inputElement.style.display = "none";
     choiceABtn.classList.add("hidden");
     choiceBBtn.classList.add("hidden"); 
     //alt text colors: #E0FEF2 , #CBFEEA . alt background colors: #3E0000
-        scene1Alt = new Scene(`#400020`, `#67FCC2`, `This opportunity is once in a lifetime.  I don't even care if I get reprimanded, PAL. I have to take a quick detour to investigate!`,
+        scene1Alt = new Scene(`#400020`, `#67FCC2`, `PAL: Welcome to Milky Way Section X216, Anomaly of unknown origin.  By my calculations, this is an active superheated gaseous nebula spinning 36 million
+        times per second and generating a fine cosmic dust containing every element in the periodic table, including 11 not yet identified.`,
         `photo by Aldebaran S on Unsplash`, `/Users/casvalkyriespicer/Documents/GitHub/oop-space-odyssey/pics/aldebaran-nebula.jpeg`, `SECTION X216: 90 Thousand Light Years from the X216 Anomaly`);
     setTimeout(function() {
         scene1Alt.fadeIn();
     }, 2500);
     setTimeout(function() {
-        scene1Alt.changeP();
-    }, 2500)
+        scene1Alt.changeP()}, 5500);
 
-    paragraph.addEventListener("mouseover", function(e) {
-        e.preventDefault();
+    setTimeout(function(){
              scene1Alt.addBackgroundImage()
-             paragraph.textContent = `HAL: Welcome to Milky Way Section X216, Anomaly of unknown origin.  By my calculations, this is an active superheated gaseous nebula spinning 36 million
-             times per second and generating a find cosmic dust containing every element in the periodic table, including 11 not yet identified.`;
+             paragraph.textContent = `This opportunity is once in a lifetime.  I don't even care if I get reprimanded, PAL. I have to take a quick detour to investigate!`;
              paragraph.style.transition = "all 3s ease-in-out";
-             choiceABtn.textContent = `Take me in closer HAL.`
-             choiceBBtn.textContent = `HAL, please perform a full spectral analysis.`
+             choiceEBtn.textContent = `Take me in closer PAL.`
+             choiceFBtn.textContent = `PAL, please perform a full spectrum analysis.`
              body.removeChild(helloDiv);
-             choiceABtn.classList.remove("hidden");
-             choiceBBtn.classList.remove("hidden");
-             choiceABtn.style.margin = "0 .5em 0 .5em";
-             choiceBBtn.style.margin = "0 .5em 0 .5em";
+             choiceEBtn.classList.remove("hidden");
+             choiceFBtn.classList.remove("hidden");
+             choiceEBtn.style.margin = "0 .5em 0 .5em";
+             choiceFBtn.style.margin = "0 .5em 0 .5em";
              input.style.margin = "0";
              paragraph.style.margin = "0";
              paragraph.style.padding = "0";
              
-    })
+    }, 2500);
 
-})
+});
